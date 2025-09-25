a65b2d966934a36dd860eb2b7f378fb12a326f3b
rena0204 <rena0204@sookmyung.ac.kr>
2025-09-25 16:24:12 +0900

    04 (6)

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 
 int main(int argc, char *argv[]) 
 {
-	int time;
+	int sec;
 	
 	printf("Input seconds:");
-	scanf("%i",&time);
+	scanf("%i", &sec);
 	
-	printf("The time is %i:%i\n", time/60, time%60);
+	printf("The time is %i:%i:%i\n",
+	sec/3600,//hour
+	(sec%3600)/60,//minute
+	sec%60//second
+	);
 	
 	return 0;
 }
